a0231447edc58ce215bfd12ceeee026e3f7a960c
Alan Hernandez <alan.m.hernandez@gmail.com>
2017-06-28 11:11:55 -0400

    Integrated Tweet Button Widget

--- js/script.js
+++ js/script.js
@@ -48,5 +48,29 @@ jQuery(document).ready(function($) {
       }
     $("#quote").html(quote);
     $("#attribution").html(attributedTo);
-  })
+  });
+
+  // Javascript Widget Script
+  window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+  $(".twitter-share-button").on("hover", function() {
+    var textToTweet = $(".quote>h2").text();
+    console.log(textToTweet);
+    $(".twitter-share-button").setAttribute("data-text", textToTweet);
+  });
 });
