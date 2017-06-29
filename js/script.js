b6f37c34c4c46caa94585c46045b82068b3c8501
Alan Hernandez <alan.m.hernandez@gmail.com>
2017-06-29 18:13:07 -0400

    Worked on Tweet Button Functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
   $(".btn-new-quote").on("click", function() {
     var quote = "";
     var attributedTo = "";
@@ -52,25 +52,25 @@ jQuery(document).ready(function($) {
 
   // Javascript Widget Script
   window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
+    var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+    return t;
+  }(document, "script", "twitter-wjs"));
 
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
+  $("blockquote a").attr("data-text", "Test");
 
-  return t;
-}(document, "script", "twitter-wjs"));
-
-  $(".twitter-share-button").on("hover", function() {
+  $(".btn-new-quote").click(function() {
     var textToTweet = $(".quote>h2").text();
     console.log(textToTweet);
-    $(".twitter-share-button").setAttribute("data-text", textToTweet);
+    $("blockquote a").attr("href", "https://twitter.com/intent/tweet?original_referer=http%3A%2F%2F127.0.0.1%3A8000%2F&ref_src=twsrc%5Etfw&text=" + textToTweet + "&tw_p=tweetbutton&url=http%3A%2F%2F127.0.0.1%3A8000%2F");
+    console.log("Test");
   });
 });
